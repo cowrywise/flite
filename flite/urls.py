5da1bdbddaf78fbcdd60a05bdca0949e29e2b1cd
Adole Samuel <adoleichasamuel@gmail.com>
2021-06-06 16:24:15 +0100

    feat: cofigure URLConf for viewsets

--- flite/urls.py
+++ flite/urls.py
@@ -4,18 +4,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers as nested_routers
 from rest_framework.authtoken import views
-from .users.views import UserViewSet, UserCreateViewSet, SendNewPhonenumberVerifyViewSet
+from .users.views import UserViewSet, UserCreateViewSet, AccountViewSet, TransactionViewSet
+
+
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
-router.register(r'phone', SendNewPhonenumberVerifyViewSet)
+router.register(r'account', AccountViewSet)
+
+transaction_router = nested_routers.NestedDefaultRouter(router, r'account', lookup="transactions")
+transaction_router.register(r'transactions', TransactionViewSet, basename='user-transactions')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #   path('jet_api/', include('jet_django.urls')),
-    path('api/v1/', include(router.urls)),
+    path('api/v1/', include(router.urls + transaction_router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
--- flite/users/utils.py
+++ flite/users/utils.py
@@ -12,43 +12,14 @@ def generate_new_user_passcode():
         passcode = _passcode()
     return passcode
 
+def generate_transaction_referencecode():
+    """
+    Returns a unique reference code for transaction
+    """
+    def _reference_code():
+        return "TRANS-" + str(uuid.uuid4().int)[0:10]
+    code = _reference_code()
+    while models.Transaction.objects.filter(reference=code).exists():
+        code = _reference_code()
+    return code
 
-def send_mobile_signup_sms(phone_number, email):
-
-    status=False
-    user_passcode = generate_new_user_passcode()
-    
-    try:
-        attempted_verification_obj = models.NewUserPhoneVerification.objects.get(phone_number=phone_number)
-    except models.NewUserPhoneVerification.DoesNotExist:
-        attempted_verification_obj = None
-
-    if attempted_verification_obj:
-        attempted_verification_obj.verification_code = user_passcode
-        attempted_verification_obj.email = email
-        attempted_verification_obj.is_verified = False
-        attempted_verification_obj.save()
-    else:
-        attempted_verification_obj = models.NewUserPhoneVerification(phone_number=str(phone_number), verification_code=user_passcode,
-            is_verified=False, email=email)
-        attempted_verification_obj.save()    
-    #tasks.send_sms_verification_code.delay(phone_number, user_passcode)
-    status = True
-    return attempted_verification_obj, user_passcode
-
-
-def validate_mobile_signup_sms(phone_number, code):
-
-    try:
-        new_user_code_obj  = models.NewUserPhoneVerification.objects.get(phone_number=phone_number, verification_code=code)
-    except models.NewUserPhoneVerification.DoesNotExist:
-        new_user_code_obj = None
-
-    if new_user_code_obj:
-        if new_user_code_obj.is_verified:
-            return 0, "Code has been verified"
-        else:
-            new_user_code_obj.is_verified = True
-            new_user_code_obj.save()
-            return 1, "Code verified"
-    return 0, "The code provided is invalid. Kindly check and try again."
