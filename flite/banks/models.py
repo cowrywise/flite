51dd115eaf5c3222f3afb2247f35d20678dc4b45
Tumise <oladele.alade@gmail.com>
2021-06-07 17:54:56 +0100

    ch(transactions) addd string representation to models

--- flite/banks/models.py
+++ flite/banks/models.py
@@ -16,13 +16,16 @@ class AllBanks(BaseModel):
     class Meta:
         verbose_name_plural = "All Banks"
 
+    def __str__(self):
+        return self.name
 
-class Bank(BaseModel):
+
+class Bank(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bank = models.ForeignKey(AllBanks, on_delete=models.CASCADE)
     account_name = models.CharField(max_length=100)
     account_number = models.CharField(max_length=50)
     account_type = models.CharField(max_length=50)
 
-
-
+    def __str__(self):
+        return "{}'s {} account".format(self.owner, self.bank)
