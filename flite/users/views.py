31b0089d26afee6e88c4da94efb4ab98cabb1091
Tumise <oladele.alade@gmail.com>
2021-06-07 10:10:37 +0100

    updated users endpoints to include users list

--- flite/users/views.py
+++ flite/users/views.py
@@ -3,54 +3,67 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .models import User, NewUserPhoneVerification
 from .permissions import IsUserOrReadOnly
-from .serializers import CreateUserSerializer, UserSerializer, SendNewPhonenumberSerializer
+from .serializers import (
+    CreateUserSerializer,
+    UserSerializer,
+    SendNewPhonenumberSerializer,
+)
 from rest_framework.views import APIView
 from . import utils
 
-class UserViewSet(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  viewsets.GenericViewSet):
+
+class UserViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
     """
     Updates and retrieves user accounts
     """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsUserOrReadOnly,)
 
 
-class UserCreateViewSet(mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+class UserCreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     """
     Creates user accounts
     """
+
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = (AllowAny,)
 
 
-class SendNewPhonenumberVerifyViewSet(mixins.CreateModelMixin,mixins.UpdateModelMixin, viewsets.GenericViewSet):
+class SendNewPhonenumberVerifyViewSet(
+    mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+):
     """
     Sending of verification code
     """
+
     queryset = NewUserPhoneVerification.objects.all()
     serializer_class = SendNewPhonenumberSerializer
     permission_classes = (AllowAny,)
 
-
-    def update(self, request, pk=None,**kwargs):
+    def update(self, request, pk=None, **kwargs):
         verification_object = self.get_object()
         code = request.data.get("code")
 
         if code is None:
-            return Response({"message":"Request not successful"}, 400)    
+            return Response({"message": "Request not successful"}, 400)
 
         if verification_object.verification_code != code:
-            return Response({"message":"Verification code is incorrect"}, 400)    
+            return Response({"message": "Verification code is incorrect"}, 400)
 
-        code_status, msg = utils.validate_mobile_signup_sms(verification_object.phone_number, code)
-        
+        code_status, msg = utils.validate_mobile_signup_sms(
+            verification_object.phone_number, code
+        )
+
         content = {
-                'verification_code_status': str(code_status),
-                'message': msg,
+            "verification_code_status": str(code_status),
+            "message": msg,
         }
-        return Response(content, 200)    
+        return Response(content, 200)
