83c48c67ede6c3b076c7340c333431952fa4b133
Tumise <oladele.alade@gmail.com>
2021-06-08 12:59:37 +0100

    ch(test) added factories for users bank account and all banks

--- flite/users/test/factories.py
+++ flite/users/test/factories.py
@@ -2,17 +2,37 @@ import factory
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-
     class Meta:
-        model = 'users.User'
-        django_get_or_create = ('username',)
+        model = "users.User"
+        django_get_or_create = ("username",)
 
-    id = factory.Faker('uuid4')
-    username = factory.Sequence(lambda n: f'testuser{n}')
-    password = factory.Faker('password', length=10, special_chars=True, digits=True,
-                             upper_case=True, lower_case=True)
-    email = factory.Faker('email')
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
+    id = factory.Faker("uuid4")
+    username = factory.Sequence(lambda n: f"testuser{n}")
+    password = factory.Faker(
+        "password",
+        length=10,
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    )
+    email = factory.Faker("email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
     is_active = True
     is_staff = False
+
+
+class AllBankFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "banks.AllBanks"
+        django_get_or_create = ["name"]
+
+
+class BankFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "banks.Bank"
+        django_get_or_create = ("owner", "bank")
+
+    owner = factory.SubFactory(UserFactory)
+    bank = factory.SubFactory(AllBankFactory)
