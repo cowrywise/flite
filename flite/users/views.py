f939b82cf57c26fefd5366e9c3d6f77c49ae468d
Tumise <oladele.alade@gmail.com>
2021-06-08 13:00:42 +0100

    rf(bug) renamed bankwitdrwal serializer class in view

--- flite/users/views.py
+++ flite/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 
 from flite.banks.manager import AccountManager
 from flite.banks.serializers import BankSerializer
-from flite.transfers.serializers import BankTransferSerializer
+from flite.transfers.serializers import BankWithdrawalSerializer, BankDepositSerializer
 from flite.transfers.wallets import UserWallet
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
@@ -13,8 +13,11 @@ from rest_framework.views import APIView
 from . import utils
 from .models import NewUserPhoneVerification, User
 from .permissions import IsUserOrReadOnly
-from .serializers import (CreateUserSerializer, SendNewPhonenumberSerializer,
-                          UserSerializer)
+from .serializers import (
+    CreateUserSerializer,
+    SendNewPhonenumberSerializer,
+    UserSerializer,
+)
 
 
 class UserViewSet(
@@ -70,7 +73,7 @@ class UserViewSet(
             status=status.HTTP_200_OK,
         )
 
-    @action(detail=True, methods=["POST"], serializer_class=BankTransferSerializer)
+    @action(detail=True, methods=["POST"], serializer_class=BankDepositSerializer)
     def deposits(self, request, pk=None):
         """enables user deposit into wallet"""
         user = self.get_object()
@@ -97,7 +100,7 @@ class UserViewSet(
             status=status.HTTP_400_BAD_REQUEST,
         )
 
-    @action(detail=True, methods=["POST"], serializer_class=BankTransferSerializer)
+    @action(detail=True, methods=["POST"], serializer_class=BankWithdrawalSerializer)
     def withdrawals(self, request, pk=None):
         """enables user withdraw to bank"""
         user = self.get_object()
