e4cde600b1ebef5ed2501a057988ca398fc73841
Chima <RolandAzim@gmail.com>
2021-06-11 06:26:05 +0100

    Untrack env

--- fliteenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/orc-1/Documents/ORC-1/Web2/cowryWiseTest/cowryWiseTestSrc2/flite/fliteenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
