bcd982bde86a42927e6144c18733fed69452c2e1
Smyja <akposlive59@gmail.com>
2024-05-01 20:48:16 +0100

    fix: updated tasks

--- flite/core/tasks.py
+++ flite/core/tasks.py
@@ -4,11 +4,11 @@ from django.db.models import Sum
 from decimal import Decimal
 
 @shared_task
-def send_threshold_email(user_email, category_name, total_spending, subject):
+def send_threshold_email(user_email, category_name, total_spending, budget, subject):
     send_mail(
         subject,
-        f'Your spending for {category_name} has reached {total_spending}.',
-        'from@example.com',
+        f'Your spending for {category_name} has reached {total_spending}. The predefined threshold is {budget}.',
+        'flite@cowrywise.com',
         [user_email],
         fail_silently=False,
     )
@@ -22,13 +22,13 @@ def check_budget_threshold(instance):
     if total_spending is None:
         return
 
-    budget = Decimal(str(category.max_spend))  # Convert budget to Decimal
+    budget = Decimal(str(category.max_spend)) 
     user_email = instance.owner.email
 
     if total_spending >= budget * Decimal('0.5') and total_spending < budget:
-        send_threshold_email.delay(user_email, category.name, str(total_spending), 'Budget threshold warning')
+        send_threshold_email.delay(user_email, category.name, str(total_spending), str(budget), 'Budget threshold warning')
     elif total_spending >= budget:
-        send_threshold_email.delay(user_email, category.name, str(total_spending), 'Budget limit exceeded')
+        send_threshold_email.delay(user_email, category.name, str(total_spending), str(budget), 'Budget limit exceeded')
 
 def check_budget_threshold_signal(sender, instance, **kwargs):
     check_budget_threshold(instance)
